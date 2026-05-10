0306f85cd11239e1b3975b018e0e315927e3b9ea
Radingka-UB <radingkaarfian@student.ub.ac.id>
2026-05-10 11:22:26 +0700

    Refactor MusicPlayer class to extend Thread and include PlayerState management

--- src/WhatLyric/Thread/MusicPlayer.java
+++ src/WhatLyric/Thread/MusicPlayer.java
@@ -1,5 +1,15 @@
 package WhatLyric.Thread;
 
-public class MusicPlayer{
-    
+import WhatLyric.Resource.PlayerState;
+import WhatLyric.Model.Music;
+import WhatLyric.Utils.TimeFormatter;
+
+public class MusicPlayer extends Thread{
+    private PlayerState state;
+    public MusicPlayer(PlayerState state){
+        this.state=state;
+    }
+    public void run(){
+        
+    }
 }
