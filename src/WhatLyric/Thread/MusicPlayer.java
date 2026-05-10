f455bd0488df185776309246562aa62c5aecdaff
Radingka-UB <radingkaarfian@student.ub.ac.id>
2026-05-10 20:30:32 +0700

    Refactor MusicPlayer class to streamline run method and remove unused code

--- src/WhatLyric/Thread/MusicPlayer.java
+++ src/WhatLyric/Thread/MusicPlayer.java
@@ -1,38 +1,29 @@
 package WhatLyric.Thread;
 
 import WhatLyric.Resource.PlayerState;
-<<<<<<< HEAD
-import WhatLyric.Model.Music;
-=======
->>>>>>> 40ac271c21b5ad2fd26e7b9db4ba80cad8c330b9
 
-public class MusicPlayer extends Thread {
-    private final PlayerState state;
-
-    public MusicPlayer(PlayerState state) {
-        this.state = state;
+public class MusicPlayer extends Thread{
+    private PlayerState state;
+    public MusicPlayer(PlayerState state){
+        this.state=state;
     }
-
-    @Override
-    public void run() {
-        System.out.println("MusicPlayer started.");
-        while (!isInterrupted()) {
-            try {
-                if (state.getState() == PlayerState.State.PLAYING) {
+    public void run(){
+        while(state.isRunning()){
+            if(state.getState()==PlayerState.State.PLAYING){
+                try{
                     Thread.sleep(1000);
-                    state.tick();
-                    
-                    Music current = state.getCurrentMusic();
-                    if (current != null && state.getCurrentPositionSeconds() >= current.getDurationSeconds()) {
-                        System.out.println("Track finished, moving to next");
-                        state.nextTrack();
-                    }
-                } else {
-                    Thread.sleep(200);
+                }catch(InterruptedException e){
+                    break;
                 }
-            } catch (InterruptedException e) {
-                System.out.println("MusicPlayer interrupted.");
-                break;
+
+                state.tick();
+
+            }else{
+                try{
+                    Thread.sleep(200);
+                }catch(InterruptedException e){
+                    break;
+                }
             }
         }
     }
