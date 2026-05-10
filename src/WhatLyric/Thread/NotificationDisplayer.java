a488e8f2b56fb37e2d1d4c755fc0166b8e92dfd0
Radingka-UB <radingkaarfian@student.ub.ac.id>
2026-05-10 11:33:23 +0700

    Implement NotificationDisplayer class with PlayerState management

--- src/WhatLyric/Thread/NotificationDisplayer.java
+++ src/WhatLyric/Thread/NotificationDisplayer.java
@@ -1,5 +1,15 @@
 package WhatLyric.Thread;
 
+import WhatLyric.Resource.PlayerState;
+
 public class NotificationDisplayer {
-    
+    private PlayerState state;
+
+    public NotificationDisplayer(PlayerState state){
+        this.state=state;
+    }
+
+    public void run(){
+        
+    }
 }
