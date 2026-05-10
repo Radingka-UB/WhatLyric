d06221ef4257bc9265ac68971c96f7178bc354b7
Radingka-UB <radingkaarfian@student.ub.ac.id>
2026-05-10 11:38:11 +0700

    Add ControlDisplayer class with PlayerState and Playlist management

--- src/WhatLyric/Thread/ControlDisplayer.java
+++ src/WhatLyric/Thread/ControlDisplayer.java
@@ -1,5 +1,21 @@
 package WhatLyric.Thread;
 
+import WhatLyric.Resource.PlayerState;
+import WhatLyric.Model.Playlist;
+import WhatLyric.Utils.TimeFormatter;
+import java.util.Scanner;
+
 public class ControlDisplayer {
-    
+    private PlayerState state;
+    private Playlist playlist;
+    private Scanner scanner;
+    public ControlDisplayer(PlayerState state,Playlist playlist){
+        this.state=state;
+        this.playlist=playlist;
+        this.scanner=new Scanner(System.in);
+    }
+
+    public void run(){
+        
+    }
 }
